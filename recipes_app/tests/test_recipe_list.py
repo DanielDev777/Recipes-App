98a6ea5ff319f9452113c3dacf3fb24b5320ee7d
Daniel Kersten <daniel@dev-kersten.de>
2026-04-28 07:54:51 +0200

    feat: added post recipe happy path test

--- recipes_app/tests/test_recipe_list.py
+++ recipes_app/tests/test_recipe_list.py
@@ -1,16 +1,33 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 class RecipeListHappyTestCase(APITestCase):
     """Happy path tests for /recipes-list/ endpoint"""
+
+    def setUp(self):
+        self.url = reverse('recipe-list')
     
     def test_get_recipes_list(self):
-        url = reverse('recipe-list')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_post_recipe(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        token = Token.objects.create(user=user)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        data = {
+            'title': 'Authenticated Recipe',
+            'description': 'Created by authenticated user',
+            'author': user.id
+        }
+        response = client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 class RecipeListUnhappyTestCase(APITestCase):
     """Unhappy path tests for /recipes-list/ endpoint"""
