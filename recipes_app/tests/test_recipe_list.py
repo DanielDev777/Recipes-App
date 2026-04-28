46fad77f9a93beade6584ee13df66541879e5169
Daniel Kersten <daniel@dev-kersten.de>
2026-04-28 07:46:50 +0200

    refactored tests to happy and unhappy paths

--- recipes_app/tests/test_recipe_list.py
+++ recipes_app/tests/test_recipe_list.py
@@ -2,23 +2,24 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-class RecipeListTests(APITestCase):
-
-    def setUp(self):
-        self.url = reverse('recipe-list')
+class RecipeListHappyTestCase(APITestCase):
+    """Happy path tests for /recipes-list/ endpoint"""
     
     def test_get_recipes_list(self):
-        response = self.client.get(self.url)
+        url = reverse('recipe-list')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_post_recipe_creates_recipe(self):
-        user = User.objects.create_user(username='testuser', password='testpass123')
-        
+class RecipeListUnhappyTestCase(APITestCase):
+    """Unhappy path tests for /recipes-list/ endpoint"""
+    
+    def test_post_recipe_without_auth(self):
+        url = reverse('recipe-list')
         data = {
             'title': 'Test Recipe',
             'description': 'A test recipe description',
-            'author': user.id
         }
-        response = self.client.post(self.url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
